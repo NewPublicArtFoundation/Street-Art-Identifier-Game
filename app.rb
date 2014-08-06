a7d1885e7bfcf3c3ce3cac4a473689b3c99febfb
RememberLenny <leonard_bogdonoff@condenast.com>
2014-08-06 11:33:36 -0400

    Save the new api

--- app.rb
+++ app.rb
@@ -2,9 +2,24 @@ require 'rubygems'
 require 'bundler'
 Bundler.require
 require './database.rb'
+require 'json'
+require 'net/http'
+
+
 
 set :root, File.dirname(__FILE__)
 
 get '/' do
   send_file 'index.html'
+end
+
+get '/get_graffiti' do
+  content_type :json
+  url = URI.parse('http://www.publicartfound.com/api/return_a_graffiti.json')
+  req = Net::HTTP::Get.new(url.to_s)
+  res = Net::HTTP.start(url.host, url.port) {|http|
+    http.request(req)
+  }
+
+  { :data => res.body }.to_json
 end
